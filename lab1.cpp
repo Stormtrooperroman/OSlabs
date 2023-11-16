8863fc8ba52a32b30c3705f5e4c996a9f37bcde5
Stormtrooperroman <stormtrooperroman@gmail.com>
2023-11-16 22:41:02 +0700

    Modify lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -10,37 +10,38 @@ using namespace std;
 mutex globalMutex;
 condition_variable condVar;
 
-bool ready = true;
+bool ready = false;
 bool predT() { return ready == true; }
-bool predF() { return ready == false; }
 int hub;
 list<int> startList = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 list<int> endList;
 
 void Provide(list<int> x) {
     while (!x.empty()) {
-        unique_lock<mutex> uniqueLock1(globalMutex, defer_lock);
-        uniqueLock1.lock();
-        condVar.wait(uniqueLock1, predT);
+        unique_lock<mutex> uniqueLock1(globalMutex);
+        while (ready) {
+            condVar.wait(uniqueLock1);
+        }
         this_thread::sleep_for(chrono::seconds(1));
         int temp = *x.begin();
         x.pop_front();
         hub = temp;
         cout << "Sended element: " << hub << "   Thread id: " \
         << this_thread::get_id() << "\n";
-        ready = false;
+        ready = true;
         condVar.notify_all();
+        uniqueLock1.unlock();
     }
 }
+
 void Consume() {
     while (endList.size() != startList.size()) {
-        unique_lock<mutex> uniqueLock2(globalMutex, defer_lock);
-        uniqueLock2.lock();
-        condVar.wait(uniqueLock2, predF);
+        unique_lock<mutex> uniqueLock2(globalMutex);
+        condVar.wait(uniqueLock2, predT);
         endList.push_front(hub);
         cout << "Recieved element: " << *endList.begin() << "   Thread id: " \
         << this_thread::get_id() << "\n";
-        ready = true;
+        ready = false;
         condVar.notify_all();
     }
 }
